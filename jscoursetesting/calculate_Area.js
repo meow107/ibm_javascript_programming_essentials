bb13af294ffa3599d7df9c3926aaf5e37457ced3
meow <46396000+meow107@users.noreply.github.com>
2025-09-18 17:19:40 +0700

    - add groceryTracker

--- jscoursetesting/calculate_Area.js
+++ jscoursetesting/calculate_Area.js
@@ -1,11 +1,22 @@
-let length;
-let width;
 
 function calculateArea() {
-    length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('width').value);
+    let length = parseFloat(document.getElementById('length').value);
+    let width = parseFloat(document.getElementById('width').value);
 
     let area = length * width;
 
-    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+    document.getElementById('area').innerText = `The area of the rectangle is: ${area}`;
 }
+
+
+function groceryTracker() {
+    let grocery1 = parseFloat(document.getElementById('grocery1').value);
+    let grocery2 = parseFloat(document.getElementById('grocery2').value);
+    let grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    let grocery_total = grocery1 + grocery2 + grocery3;
+
+    document.getElementById('grocery_total').innerText = `The total amount is: ${grocery_total} $`;
+}
+
+
